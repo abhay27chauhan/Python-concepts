1f7609b2bf19fe0bfa45bee3120af5f58bb63200
Abhay <chauhan27.abhay@gmail.com>
2021-01-02 02:06:55 +0530

    operators

---
+++ operator.py
@@ -0,0 +1,18 @@
+# operations in python
+
+2 + 3
+7 - 4
+3 * 2
+6 / 3  # 2.0 -> always give float
+2 ** 3 # 2*2*2
+
+'''
+operators precedence -
+
+1. () -> parentheses
+2. **  -> exponents
+3. * / -> both have same precedence, anyone of the two operators coming 1st in  
+          equation will be given priority.
+4. + - -> both have same precedence, anyone of the two operators coming 1st in 
+          equation will be given priority.
+'''
