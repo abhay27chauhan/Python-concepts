3c95e520ac0ff1c7766bd9eb526103e8ba807dff
Abhay <chauhan27.abhay@gmail.com>
2021-01-10 00:00:42 +0530

    control flow

---
+++ controlFlow.py
@@ -0,0 +1,16 @@
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+bmi = height/ weight ** 2
+bmi = round(bmi, 2)
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you are normalweight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
